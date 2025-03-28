5fb6474defab012ac1212e63c8b59b870b1cb0e5
Jp <pyjean15@gmail.com>
2025-03-28 15:50:33 +0200

    modele version 1

--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    nom=models.CharField(max_length=255,unique=True)
+    description=models.TextField(blank=True,null=True)
+    categorie=models.CharField(max_length=100)
+    type=models.CharField(max_length=100,blank=True,null=True)
+    forme=models.CharField(max_length=100,blank=True,null=True)
+    couleur=models.CharField(max_length=100,blank=True,null=True)
+    prix_achat=models.DecimalField(max_digits=10,decimal_places=2)
+    prix_vente=models.DecimalField(max_digits=10,decimal_places=2)
+    seuil_areaprovisionnement=models.PositiveIntegerField(default=10)
+
+    def stock_total(self):
+        return sum(entry.quantite for entry in self.stock_entries.all())
+    def __str__(self):
+        return f"{self.nom}({self.stock_total()} en stock)"
+class StockEntry(models.Model):
+    article=models.ForeignKey(Article,related_name="stock_entries",on_delete=models.CASCADE)
+    quantite=models.PositiveIntegerField()
+    date_entree=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.article.nom}-{self.quantite} unité entrées le{self.date_entree}"
+    
+class StockMouvement(models.Model):
+    article=models.ForeignKey(Article,on_delete=models.CASCADE)
+    quantite=models.PositiveIntegerField()
+    date_sortie=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.article.nom}-{self.quantite} unités sortie le{self.date_sortie}"
